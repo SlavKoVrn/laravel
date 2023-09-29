bae0118983cdd040a640cc1f31c2495ff9a2d042
SlavKoVrn <slavko.chita@gmail.com>
2023-09-29 15:32:37 +0300

    sort

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Comment;
+use App\Models\Like;
 use App\Http\Controllers\AbstractController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -19,8 +20,45 @@ class CommentController extends AbstractController
      */
     public function index(Request $request)
     {
-        $comments = Comment::with(['user','likes'])
-            ->paginate($request->get('per_page'));
+        $input = $request->all();
+        $rules = [
+            'sort' => 'nullable|in:user_id,-user_id,create_at,-create_at,max_count_likes_code,-max_count_likes_code',
+        ];
+
+        $validator = Validator::make($input, $rules);
+
+        if($validator->fails()){
+            return response()->json([
+                "success" => false,
+                "message" => 'Validation Error.',
+                "data" => $validator->errors()
+            ]);
+        }
+
+        $qb = Comment::query();
+        $qb->select([
+            'comments.id as comment_id',
+            'title',
+            'comments.user_id',
+            'comments.created_at',
+            'comments.updated_at'
+        ])
+            ->selectRaw('MAX(likes.code) * COUNT(likes.code) AS max_count_likes_code')
+            ->join('users', 'users.id', '=', 'comments.user_id')
+            ->join('likes', 'comments.id', '=', 'likes.comment_id')
+            ->groupBy('comments.id');
+
+        if($request->has('sort')){
+            $sort = $request->get('sort');
+            $trimSorm = trim($sort,'-');
+            if (substr($sort,0,1) == '-'){
+                $qb->orderBy($trimSorm,'DESC');
+            }else{
+                $qb->orderBy($trimSorm,'ASC');
+            }
+        }
+
+        $comments = $qb->paginate($request->get('per_page'));
 
         return response()->json([
             "success" => true,
